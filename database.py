5e1fb55f096dc04efb4fa8b7f3e9b98679536366
Jhonata Almeida <yorn.exe@gmail.com>
2025-08-29 14:15:53 +0000

    Primeira versão MVC de frameworks

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine('sqlite:///users.db', connect_args={"check_same_thread": False})
+db_session = scoped_session(sessionmaker(bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
